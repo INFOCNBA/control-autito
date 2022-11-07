6fa5efb4ac9016c24a2e9e036f0a4328516abacf
Informática CNBA <105891917+INFOCNBA@users.noreply.github.com>
2022-11-07 16:45:42 -0300

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,21 +1,42 @@
+#
+#   Este programa es parte del enunciado del Trabajo Final
+#
+#   Ver los "comentarios" que autodocumentan éste programa
+#        (lo que viene después de cada "#")
+
+#
+# Sección de Variables Globales
+#
 grupo_radio=21
 comandos_validos=["izquierda","derecha","adelante","atrás"] #comandos en "texto claro"
 
+#
+# Sección de funciones y procedimientos
+#
 def encriptar(comando:str):
-    resultado=""
-    for letra in comando:
-        resultado+=letra#[indice]
-    return resultado
+    #
+    # ésta función implementa la "encriptación" del autito
+    # devuelve las primeras tres consonantes del <comando> de entrada
+    #   ejemplo: si recibe "fruta" devuelve "frt"
+    #
+    #
+    # COMPLETAR: por ahora devueve la misma cadena que recibe...
+    #             o sea... "encripta??" nah...
+    #            Es tu trabajo que ésta función cumpla el enunciado :)
+    #
+    resultado="" #comenzar con una variable tipo Cadena, vacía
+    for letra in comando: #por cada letra en el parámetro de entrada<comando>
+        resultado += letra #agregar la letra a la variable <resultado>
+    return resultado #devolver el resultado
 
-def on_received_string(comando):
+def procesar_recepcion(comando):
     basic.show_string(comando)
     #return None
 
 def enviar_por_radio(comando):
-    comando=str(comando)
-    comando_encriptado = encriptar(comando)
+    comando_encriptado = encriptar(comando) #llamamos a la funcion "encriptar"
     radio.send_string(comando_encriptado)
-    basic.clear_screen()
+    basic.clear_screen() #borramos la pantalla para avisar del envío
     #return None
 
 def interfaz_de_usuario_a():  #izquierda o atrás
@@ -25,6 +46,6 @@ def interfaz_de_usuario_a():  #izquierda o atrás
     enviar_por_radio(comando)
     #return None
 
-radio.on_received_string(on_received_string)
+radio.on_received_string(procesar_recepcion)
 input.on_button_pressed(Button.A, interfaz_de_usuario_a)
 radio.set_group(grupo_radio)
--- main.ts
+++ main.ts
@@ -1,26 +1,51 @@
+// 
+//    Este programa es parte del enunciado del Trabajo Final
+// 
+//    Ver los "comentarios" que autodocumentan éste programa
+//         (lo que viene después de cada "#")
+// 
+//  Sección de Variables Globales
+// 
 let grupo_radio = 21
 let comandos_validos = ["izquierda", "derecha", "adelante", "atrás"]
 // comandos en "texto claro"
+// 
+//  Sección de funciones y procedimientos
+// 
 function encriptar(comando: string): string {
+    // 
+    //  ésta función implementa la "encriptación" del autito
+    //  devuelve las primeras tres consonantes del <comando> de entrada
+    //    ejemplo: si recibe "fruta" devuelve "frt"
+    // 
+    // 
+    //  COMPLETAR: por ahora devueve la misma cadena que recibe...
+    //              o sea... "encripta??" nah...
+    //             Es tu trabajo que ésta función cumpla el enunciado :)
+    // 
     let resultado = ""
+    // comenzar con una variable tipo Cadena, vacía
     for (let letra of comando) {
+        // por cada letra en el parámetro de entrada<comando>
         resultado += letra
     }
-    // [indice]
+    // agregar la letra a la variable <resultado>
     return resultado
 }
 
+// devolver el resultado
 // return None
 function enviar_por_radio(comando: string) {
-    comando = "" + comando
     let comando_encriptado = encriptar(comando)
+    // llamamos a la funcion "encriptar"
     radio.sendString(comando_encriptado)
     basic.clearScreen()
 }
 
+// borramos la pantalla para avisar del envío
 // return None
 // return None
-radio.onReceivedString(function on_received_string(comando: string) {
+radio.onReceivedString(function procesar_recepcion(comando: string) {
     basic.showString(comando)
 })
 input.onButtonPressed(Button.A, function interfaz_de_usuario_a() {
